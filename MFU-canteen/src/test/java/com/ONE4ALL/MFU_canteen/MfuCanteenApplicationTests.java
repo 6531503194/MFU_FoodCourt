6ce9a2bb9cb21b9d277cfb491aeef7ad4c6234d7
Yoon Moh Moh Aung <122962865+6531503194@users.noreply.github.com>
2024-10-16 23:20:43 +0700

    Add files via upload

---
+++ MFU-canteen/src/test/java/com/ONE4ALL/MFU_canteen/MfuCanteenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ONE4ALL.MFU_canteen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfuCanteenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
